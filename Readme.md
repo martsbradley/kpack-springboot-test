6789411546ccc7c890f79f9d51d14ceebad73219
martin <martin@atom>
2025-06-23 10:09:01 +0100

    Initial and final version

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Just the most basic Springboot program
+that I can create for running kpack on k8s.
---
+++ src/test/java/com/martsbradley/kpack/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martsbradley.kpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
